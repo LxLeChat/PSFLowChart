eb1c223c41b58114fff6ca8f25c1fe9d02844ee1
Lx <yanaski@msn.com>
2019-11-06 23:54:53 +0100

    upload graph example avec un switch

--- plop.ps1
+++ plop.ps1
@@ -10,20 +10,27 @@ If ( $b ) {
             "BAAAAAAAAAAH"
         }
     
-        # foreach ( $caca in $prout ) {
-        #     "cacaprout"
-        #     if ( $caca ) {
-        #         "rien"
-        #     } elseif ($prout) {
-        #         foreach ( $plop in $plip ) {
-        #             "cacca"
-        #         }
-        #     }
-        # }
+        foreach ( $caca in $prout ) {
+            "cacaprout"
+            if ( $caca ) {
+                "rien"
+            } elseif ($prout) {
+                foreach ( $plop in $plip ) {
+                    "cacca"
+                }
+            }
+        }
 
-        # while ( $i -lt 10 ){
-        #     "ahahah"
-        # }
+        while ( $i -lt 10 ){
+            switch ( $i ) {
+                1 { "aaaa"}
+                2 { "aaaa"}
+                3 { "aaaa"}
+                4 { "aaaa"}
+                5 { "aaaa"}
+                default { "bbbbb"}
+            }
+        }
             
         }
 }
