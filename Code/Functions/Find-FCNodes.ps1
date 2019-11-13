2b80273da17c9d1a4841ca82e06edefc566ac167
LxLeChat <yanaski@msn.com>
2019-11-13 13:41:24 +0100

    Update Find-FCNodes.ps1

--- Code/Functions/Find-FCNodes.ps1
+++ Code/Functions/Find-FCNodes.ps1
@@ -1,34 +1,65 @@
-function Find-FCNodes {
+function Find-FCNode {
     <#
     .SYNOPSIS
-        Short description
+        Find "nodes" present in script
     .DESCRIPTION
-        Long description
+        Find "nodes" present in script
     .EXAMPLE
-        PS C:\> <example usage>
-        Explanation of what the example does
+        PS C:\> Find-FCNode -File .\basic_example_1.ps1
+
+        Type        : If
+        Statement   : If ( $a -eq 10 )
+        Description :
+        Children    : {ForeachNode, ElseNode}
+        Parent      :
+        Depth       : 1
+        File        : C:\basic_example_1.ps1
+
+        Return all the nodes present in the basic_example_1.ps1
     .INPUTS
-        Inputs (if any)
+        ps1 file path
     .OUTPUTS
-        Output (if any)
+        [node[]]
     .NOTES
-        General notes
+        Pipeline is accepted, so Gci c:\temp -filter "*.ps1" | Find-FCNode should Work
     #>
 
     [CmdletBinding()]
     param (
-        [String]$File,
-        [Switch]$FindDescription
+        # Parameter help description
+        [Parameter(Mandatory=$True,
+        ValueFromPipelineByPropertyName=$True,Position=1)]
+        [Alias("FullName")]
+        [String[]]
+        $File,
+        # Whether you want ton find associated node description
+        [Parameter(Mandatory=$False,ParameterSetName='Description')]
+        [Switch]
+        $FindDescription,
+        # The KeyWord representing the begining of your comment, default: Description
+        [Parameter(Mandatory=$False,ParameterSetName='Description')]
+        [String]
+        $KeyWord = $null
     )
     
     begin {
-        
+        ## Check if PSGRAPH is loaded or available ?
     }
     
     process {
-        $FilePath = Get-Item $File
-        $x=[nodeutility]::ParseFile($FilePath.FullName)
+
+        $FileInfo = Get-Item $File
+        $x=[nodeutility]::ParseFile($FileInfo.FullName)
+
+        If ( $FindDescription ) {
+            If ( $KeyWord ) {
+                $X.FindDescription($True,$KeyWord)
+            } Else {
+                $X.FindDescription($True)
+            }
+        }
         return ,$x
+
     }
     
     end {
