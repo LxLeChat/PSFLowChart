137e75efcc2354f55f33c87d49a278692b437a27
Lx <yanaski@msn.com>
2019-11-16 00:35:28 +0100

    cleaning

--- DirtyBuild.ps1
+++ DirtyBuild.ps1
@@ -1,6 +1,6 @@
-Remove-Item .\psflowchart.psm1 -ErrorAction SilentlyContinue
-Get-Content .\Code\Classes\classes.psm1 >> .\psflowchart.psm1
+Remove-Item .\PSFlowchart\psflowchart.psm1 -ErrorAction SilentlyContinue
+Get-Content .\PSFlowchart\Code\Classes\classes.psm1 >> .\PSFlowchart\PSFlowchart.psm1
 
-Foreach ( $File in (Gci .\Code\Functions -Filter *.ps1) ) {
-    Get-Content $File.FullName -raw >> .\psflowchart.psm1
+Foreach ( $File in (Gci .\PSflowchart\Code\Functions -Filter *.ps1) ) {
+    Get-Content $File.FullName -raw >> .\PSFlowchart\PSFlowchart.psm1
 }
