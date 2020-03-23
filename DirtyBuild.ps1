f6d5405c23da6a7aa097c068f8436e5cccac91f9
LxLeChat <yanaski@msn.com>
2020-03-23 15:23:56 +0100

    Update DirtyBuild.ps1

--- DirtyBuild.ps1
+++ DirtyBuild.ps1
@@ -1,6 +1,6 @@
-Remove-Module psflowchart -Force -ErrorAction SilentlyContinue
-Remove-Item .\PSFlowchart\psflowchart.psm1 -ErrorAction SilentlyContinue
-Get-Content .\PSFlowchart\Code\Classes\classes.psm1 >> .\PSFlowchart\PSFlowchart.psm1
+#Remove-Module psflowchart -Force -ErrorAction SilentlyContinue
+#Remove-Item .\PSFlowchart\psflowchart.psm1 -ErrorAction SilentlyContinue
+Get-Content .\PSFlowchart\Code\Classes\classes.psm1 > .\PSFlowchart\truc.psm1
 
 Foreach ( $File in (Gci .\PSflowchart\Code\Functions -Filter *.ps1) ) {
     Get-Content $File.FullName -raw >> .\PSFlowchart\PSFlowchart.psm1
